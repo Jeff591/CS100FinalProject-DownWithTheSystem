664ec7ab95981485d930a88e8eaa936e96916a32
CameronSF <cfong027@ucr.edu>
2021-11-12 14:55:06 -0800

    Adding weapons, armor, and potions

--- Armor.hpp
+++ Armor.hpp
@@ -20,4 +20,69 @@ class Armor : public Item
     
 };
 
+class ByteArmor : public Armor
+{
+  public:
+    ByteArmor()
+    {
+      sell_price = 15;
+      name = "ByteArmor";
+      description = "Armor made of a byte of code. Probably too thin to be proper armor but its better than nothing!";
+      defense = 5;
+    }
+    
+};
+
+class KiloArmor : public Armor
+{
+  public:
+    KiloArmor()
+    {
+      sell_price = 30;
+      name = "KiloArmor";
+      description = "Armor made of a kilobyte of code. This weighs a kilo! You'll definitely gain strength carrying this!";
+      defense = 10;
+    }
+    
+};
+
+class MegaArmor : public Armor
+{
+  public:
+    MegaArmor()
+    {
+      sell_price = 60;
+      name = "MegaArmor";
+      description = "Armor made of a megabyte of code. Being able to shoot and change weapons like MegaMan not included";
+      defense = 15;
+    }
+    
+};
+
+class GigaArmor : public Armor
+{
+  public:
+    GigaArmor()
+    {
+      sell_price = 120;
+      name = "GigaArmor";
+      description = "Armor made of a gigabyte of code. Might give giga defense physically, but it'll be a giga dent on your money";
+      defense = 20;
+    }
+    
+};
+
+class TeraArmor : public Armor
+{
+  public:
+    TeraArmor()
+    {
+      sell_price = 240;
+      name = "TeraArmor";
+      description = "Legendary armor made of a terabyte of code. The best of the best armor around the interface, but as it is the best, only a few exist due to the immense work for its creation.";
+      defense = 25;
+    }
+    
+};
+
 #endif
--- Potion.hpp
+++ Potion.hpp
@@ -21,4 +21,18 @@ class Potion : public Item
     
 };
 
+class HealthPotion : public Potion
+{
+  public:
+    HealthPotion()
+    {
+      sell_price = 5;
+      name = "HealthPotion";
+      description = "A nice drink filled to the brim with dihydrogen monoxide or as you people call it, water. It'll replenish you well.";
+      type = "Health";
+      alter = 15;
+    }
+    
+};
+
 #endif
